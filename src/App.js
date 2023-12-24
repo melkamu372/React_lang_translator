520d8bee874dc77f176cbce96956bc36004bf0e7
melkamu372 <melkamu372@gmail.com>
2023-12-24 20:41:54 +0300

    language translator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="description">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+
+
+         <b> </b> {t('edit')}
         </p>
         <a
           className="App-link"
@@ -15,7 +32,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {t('description')}
         </a>
       </header>
     </div>
---
+++ src/local/translations.js
@@ -0,0 +1,11 @@
+import en from "../local/en_us"
+import den from "../local/ge"
+const translations = {
+    en: {
+      translation: en,
+    },
+    de: {
+      translation: den,
+    },
+  };
+  export default translations;
